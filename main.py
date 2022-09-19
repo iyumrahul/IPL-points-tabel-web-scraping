c82e0d9f422e2711771a9f133dd5dd77500f30c6
Rahul <107238271+iyumrahul@users.noreply.github.com>
2022-09-19 11:33:30 +0000

    adding 3rd column

--- main.py
+++ main.py
@@ -25,38 +25,48 @@ if __name__ == "__main__":
     print("##########################")
 
     # get Point list tabel by class name
-    outer_tag=driver.find_elements(By.CLASS_NAME,'ih-pcard-wrap')
+    outer_tag=driver.find_elements(By.CLASS_NAME,'ih-pt-tab-bg')
     
     outerTag=outer_tag[0]
-    
-    inner_tag_col1=outerTag.find_elements(By.CLASS_NAME,'table-qualified')
 
+    inner_tag_col1=outerTag.find_elements(By.CLASS_NAME,'table-qualified')
     inner_tag_col2=outerTag.find_elements(By.CLASS_NAME,'ih-t-color')
     
-    middle_tag=outerTag.find_elements(By.TAG_NAME,'tr')[0]
+    inner_tag_col3=outerTag.find_elements(By.TAG_NAME,'tr')
     
-    inner_tag_col3=middle_tag.find_elements(By.TAG_NAME,'td')
-    
-
-    print("###########################")
-
     position=[]
     
-
     for col1 in inner_tag_col1:
       print(col1.text)
-    
     print("##########################")
-
+    
     for col2 in inner_tag_col2:
       print(col2.text)
+    print("###########################")
+
+    for col3 in inner_tag_col3:
+      print(col3.find_elements(By.XPATH,"//td[@class='ng-binding']")[0].text)
 
     print("###########################")
-    print(inner_tag_col3)
-    
-    
-    
+    for col4 in inner_tag_col3:
+      print(col4.find_elements(By.XPATH,"//td[@class='ng-binding']")[1].text)
+    for col5 in inner_tag_col3:
+      print(col5.find_elements(By.XPATH,"//td[@class='ng-binding']")[2].text)
+    for col6 in inner_tag_col3:
+      print(col6.find_elements(By.XPATH,"//td[@class='ng-binding']")[3].text)
+    for col7 in inner_tag_col3:
+      print(col7.find_elements(By.XPATH,"//td[@class='ng-binding']")[4].text)
+    for col8 in inner_tag_col3:
+      print(col8.find_elements(By.XPATH,"//td[@class='ng-binding']")[5].text)
+    for col9 in inner_tag_col3:
+      print(col9.find_elements(By.XPATH,"//td[@class='ng-binding']")[6].text)
+    for col10 in inner_tag_col3:
+      print(col10.find_elements(By.XPATH,"//td[@class='ng-binding']")[7].text)
+    for col11 in inner_tag_col3:
+      print(col11.find_elements(By.XPATH,"//td[@class='ng-binding']")[8].text)
       
+    
+    
     
    
    
